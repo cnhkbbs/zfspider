9733b225646c08444c3aafa07757489e8cecc165
bui~bui~bui~bui <53457442+cnhkbbs@users.noreply.github.com>
2023-07-18 12:02:39 +0800

    Update cookfood.py

--- cookfood.py
+++ cookfood.py
@@ -9,10 +9,12 @@ from bs4 import BeautifulSoup as bf
 
 def CookFood(username):
     try:
-        with open('scores/' + username + '.txt', 'r', encoding='utf-8') as file:
+        with open(username + '.txt', 'r', encoding='utf-8') as file:
             score_response = file.read()
     except FileNotFoundError:
-        return {'massage': 'FileNotFoundError', 'status': False}
+        return "返回结果失败，系统中没有对应记录！"
+    except Exception as e:
+        return "未知错误：" + str(e)
     soup = bf(score_response, 'html.parser')
     score_table = soup.find('table', {"class": "datelist"})
     html_header = '<!DOCTYPE html><html><head><meta charset="utf-8" /><title></title><style>td{border: 1px solid black!important;}</style></head><body>'
